fca00268d454abc7473ea1f018ce20d2fe8aa45b
Erik <foxmayz@gmx.de>
2022-09-26 17:52:09 +0200

    added gap

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <footer className=" bg-gray-700 dark:bg-gray-900  text-white ">
-      <div className="max-w-[1280px] gap-row-5 h-20  px-8 flex justify-between items-center flex-wrap mx-auto sm:justify-center ">
+      <div className="max-w-[1280px] gap-x-5 h-20  px-8 flex justify-between items-center flex-wrap mx-auto sm:justify-center ">
         <p className="h-6">© 2022 K&K Coding</p>
         <div className="flex gap-5 h-6">
           <Link to="impressum">Impressum</Link>
