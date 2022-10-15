60162057587b5d66dd7a88f02158e03e3b44801e
Erik <foxmayz@gmx.de>
2022-10-15 11:28:54 +0200

    change images and alt tags, remove unused css

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,6 @@ import PrivacyProtection from "./components/PrivacyProtection";
 import Contact from "./components/Contact";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./react-i18next";
-
-import { Suspense } from "react";
 import {
   BrowserRouter as Router,
   Routes,
