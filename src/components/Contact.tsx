f3002cfd43bd747a0527db669e67e32e9ecc7a37
Erik <foxmayz@gmx.de>
2022-10-15 16:11:52 +0200

    fixed bug

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,7 +4,7 @@ import { GrMail } from "react-icons/gr";
 function Contact() {
   const { t } = useTranslation();
   return (
-    <div className=" bg-neutral-100 dark:bg-gray-600" id="contact">
+    <div className=" bg-neutral-100 dark:bg-gray-600 " id="contact">
       <div className="w-3/4 mx-auto pb-10 pt-8">
         <h3 className="text-center mb-4">{t("contact.headline")}</h3>
         <p className="text-center mb-4">{t("contact.contactText")}</p>
